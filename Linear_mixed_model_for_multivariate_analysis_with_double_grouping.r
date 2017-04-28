9b2dc7fcded88c537cf2031b74ee86b9ee89f67f
Zhongyun Huang <zhuang@cns.umass.edu>
2017-04-27 23:31:42 -0400

    Update Linear_mixed_model_for_multivariate_analysis_with_double_grouping.r

--- Linear_mixed_model_for_multivariate_analysis_with_double_grouping.r
+++ Linear_mixed_model_for_multivariate_analysis_with_double_grouping.r
@@ -45,6 +45,40 @@ fit <- lmer(glucose ~ Ances_group + Oryza_group + (1|genotype),
             data=weed, REML=FALSE)
 summary(fit)
 
+'''
+Linear mixed model fit by maximum likelihood  ['lmerMod']
+Formula: glucose ~ Ances_group + Oryza_group + (1 | genotype)
+   Data: weed
+
+     AIC      BIC   logLik deviance df.resid 
+    44.6     56.8    -15.3     30.6       35 
+
+Scaled residuals: 
+    Min      1Q  Median      3Q     Max 
+-2.7408 -0.4865 -0.2552  0.3394  2.3801 
+
+Random effects:
+ Groups   Name        Variance Std.Dev.
+ genotype (Intercept) 0.0000   0.0000  
+ Residual             0.1214   0.3484  
+Number of obs: 42, groups:  genotype, 14
+
+Fixed effects:
+                   Estimate Std. Error t value
+(Intercept)         0.26750    0.11612   2.304
+Ances_groupind      0.12933    0.11612   1.114
+Ances_groupwild     0.83958    0.18361   4.573
+Oryza_groupSA_weed  0.05275    0.14222   0.371
+Oryza_groupU_weed   0.18042    0.14222   1.269
+
+Correlation of Fixed Effects:
+            (Intr) Ancs_grpn Ancs_grpw Or_SA_
+Ances_grpnd -0.500                           
+Ancs_grpwld -0.158  0.316                    
+Oryz_grpSA_ -0.612  0.000    -0.387          
+Oryz_grpU_w -0.612  0.000     0.000     0.500
+'''
+
 #for fructose in PC1
 fit <- lmer(fructose ~ Ances_group + Oryza_group + (1|genotype),
             data=weed, REML=FALSE)
